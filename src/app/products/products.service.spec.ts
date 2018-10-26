3b2058bb73f626eeb5dbe596593acfa793541435
Jeffrey <jeffny2015@gmail.com>
2018-10-25 18:46:33 -0600

    products.service.rspec.ts

--- src/app/products/products.service.spec.ts
+++ src/app/products/products.service.spec.ts
@@ -1,12 +1,32 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ProductsService } from './products.service';
+import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-describe('ProductsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe(`ProductService`, () => {
 
-  it('should be created', () => {
-    const service: ProductsService = TestBed.get(ProductsService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ]
+    });
   });
+
+  it(`should issue a request`,
+    async(
+      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+      	
+        http.get('http://localhost:3000/api/v1/products').subscribe();
+
+
+        backend.expectOne({
+          url: 'http://localhost:3000/api/v1/products',
+          method: 'GET'
+        });
+      })
+    )
+  );
+
 });
