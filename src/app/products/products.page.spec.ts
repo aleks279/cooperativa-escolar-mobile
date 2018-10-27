f206a321e84fec5ab2450e9d06bc622c2d3ff0c5
Jeffrey <jeffny2015@gmail.com>
2018-10-26 18:01:58 -0600

    some fixes

--- src/app/products/products.page.spec.ts
+++ src/app/products/products.page.spec.ts
@@ -1,5 +1,4 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-//import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { ProductsPage } from './products.page';
 import { ProductsService } from './products.service';
 import { TestBed, async, inject } from '@angular/core/testing';
@@ -7,37 +6,6 @@ import { HttpClientModule, HttpClient ,HttpHandler} from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('ProductsPage', () => {
-  /*let component: ProductsPage;
-  let fixture: ComponentFixture<ProductsPage>;
-  let productsPage: HTMLElement;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports:[],
-      declarations: [ProductsPage],
-      providers: [ProductsService,HttpClient,HttpClientModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-      .compileComponents();
-  }));
-
-
-
-  beforeEach(async () => {
-    fixture = await TestBed.createComponent(ProductsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('should have a products of 10 elements', () => {
-    productsPage = fixture.nativeElement;
-    const items = productsPage.querySelectorAll('ion-item');
-    expect(items.length).toEqual(10);
-  });*/
 
    beforeEach(() => {
     TestBed.configureTestingModule({
@@ -48,6 +16,4 @@ describe('ProductsPage', () => {
    it('should be created',inject([ProductsService,HttpClient,HttpHandler,ProductsPage],(page: ProductsPage) =>{
     expect(page).toBeTruthy();
   }));
-
-
 });
--- src/app/products/products.service.spec.ts
+++ src/app/products/products.service.spec.ts
@@ -4,46 +4,24 @@ import { HttpClientModule, HttpClient ,HttpHandler} from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe(`ProductService`, () => {
-  /*let chttp:HttpClient;
-  let p: ProductsService = new ProductsService(chttp);
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-        HttpClientTestingModule
-      ]
-    });
-  });*/
-
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ProductsService,HttpClient,HttpHandler]
     });
   });
 
-  /*it(`should issue a request`,
-    async(
-      inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
-      	var url = p.apiUrl;
-        http.get(`${url}api/v1/products`).subscribe();
-
-
-        backend.expectOne({
-          url: 'http://localhost:3000/api/v1/products',
-          method: 'GET'
-        });
-      })
-    )
-  );*/
 
   it('should be created',inject([ProductsService,HttpClient,HttpHandler],(service: ProductsService) =>{
     expect(service).toBeTruthy();
   }));
+
   it('should have getProducts',inject([ProductsService,HttpClient,HttpHandler],(service: ProductsService) =>{
     expect(service.getProducts).toBeTruthy();
   }));
+
   it('should have appiUrl',inject([ProductsService,HttpClient,HttpHandler],(service: ProductsService) =>{
     expect(service.apiUrl).toBeTruthy();
-  }));
+  }));   
 
 });
